1108fb93036f2a61cc0973d64943ff73ba5b9017
andgeral <geraldinebecerra93@gmail.com>
2024-05-15 13:39:16 -0400

    se agrega ruta gastos

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import path from "path";
 import roommate from "./routes/roommate.js";
-
+import gastos from "./routes/gastos.js";
 const { PORT } = process.env;
 const __dirname = path.resolve();
 const app = express();
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(roommate);
+app.use(gastos);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
