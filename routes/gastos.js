220b398898f587fd5b9e75b5565c21ead5b6132a
andgeral <geraldinebecerra93@gmail.com>
2024-05-15 16:48:43 -0400

    se agrega la ruta post para nuevo gasto

--- routes/gastos.js
+++ routes/gastos.js
@@ -4,5 +4,6 @@ import { gastosController } from "../controllers/gastos.js";
 const router = Router();
 
 router.get("/gastos", gastosController.findAll);
+router.post("/gasto", gastosController.create);
 
 export default router;
