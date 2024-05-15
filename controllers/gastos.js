f08605b506ff387ba4cc83a372140f3c485d4f51
andgeral <geraldinebecerra93@gmail.com>
2024-05-15 16:47:49 -0400

    se crea funcionalidad para crear un nuevo gasto

--- controllers/gastos.js
+++ controllers/gastos.js
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 const findAll = async (req, res) => {
   try {
     const { gastos } = JSON.parse(
@@ -10,6 +12,30 @@ const findAll = async (req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  const { roommate, descripcion, monto } = req.body;
+  const { gastos } = JSON.parse(
+    fs.readFileSync("./data/gastos_data.json", "utf8")
+  );
+  const id = uuidv4().slice(0, 6);
+  const fecha = moment().format("DD/MM/YYYY");
+  const newGasto = {
+    roommate,
+    descripcion,
+    monto,
+    id,
+    fecha,
+  };
+
+  gastos.push(newGasto);
+  fs.writeFileSync("./data/gastos_data.json", JSON.stringify({ gastos }));
+  res.status(201).send({
+    message: "Gasto creado con exito",
+    newGasto,
+  });
+};
+
 export const gastosController = {
   findAll,
+  create,
 };
