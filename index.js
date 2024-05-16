306876e7a87a0716cd0e5122f54dc8acbe7fd9d2
andgeral <geraldinebecerra93@gmail.com>
2024-05-15 22:40:13 -0400

    COMENTARIO

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { PORT } = process.env;
 const __dirname = path.resolve();
 const app = express();
 
+//SUBO EL PROYECTO PERO ME FALTO PARA TERMINAR NO LOGRE HACER QUE ACTUALIZARA Y TAMPOCO LO DE LOS MONTOS
 app.use(express.json());
 
 app.use(roommate);
